67f5c5698f0db55685acb8d9de997584c046a69c
H-NGT <nov.13.v3.0@gmail.com>
2022-07-30 01:16:58 +0900

    Initial Commit

---
+++ CardApp/CardAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardAppApp.swift
+//  CardApp
+//
+//  Created by 永田浩之 on 2022/07/30.
+//
+
+import SwiftUI
+
+@main
+struct CardAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
